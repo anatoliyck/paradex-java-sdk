6da630bba7a43945acd75171890b9979a25d5102
Anatolii Prokhorenko <anatoliy.ck@gmail.com>
2025-03-08 11:08:27 +0100

    add javadocs for auth API

--- src/main/java/trade/paradex/api/auth/ParadexAuthAPI.java
+++ src/main/java/trade/paradex/api/auth/ParadexAuthAPI.java
@@ -4,6 +4,12 @@ import trade.paradex.model.ParadexAccount;
 
 public interface ParadexAuthAPI {
 
+    /**
+     * Authenticate using signed payload to get a JWT for usage in other endpoints.
+     *
+     * @param account {@link ParadexAccount}
+     * @return JWT auth token
+     */
     String auth(ParadexAccount account);
 
 }
